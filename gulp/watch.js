32f850a363790a99e072375252bd77e2a6d61fef
Chris <gatto_c@yahoo.ca>
2016-01-24 14:22:55 -0400

    updated gulp watch

--- gulp/watch.js
+++ gulp/watch.js
@@ -2,6 +2,8 @@
 
 var path = require('path');
 var gulp = require('gulp');
+var watch = require('gulp-watch');
+var batch = require('gulp-batch');
 var conf = require('./conf');
 
 var browserSync = require('browser-sync');
@@ -10,30 +12,54 @@ function isOnlyChange(event) {
   return event.type === 'changed';
 }
 
-gulp.task('watch', ['scripts:watch', 'inject'], function () {
-
-  console.log('conf: ', conf);
-
-  gulp.watch([path.join(conf.paths.src, '/*.html'), 'bower.json'], ['inject-reload']);
-
-  console.log('here1');
-
-  gulp.watch([
-    path.join(conf.paths.src, '/app/**/*.css'),
-    path.join(conf.paths.src, '/app/**/*.scss')
-  ], function(event) {
-    console.log('here2');
-    if(isOnlyChange(event)) {
-      console.log('here3');
-      gulp.start('styles-reload');
-    } else {
-      console.log('here4');
-      gulp.start('inject-reload');
-    }
-  });
-
-
-  gulp.watch(path.join(conf.paths.src, '/app/**/*.html'), function(event) {
-    browserSync.reload(event.path);
-  });
+/**
+ * watch:sass
+ * Watches the SASS file and runs the 'sass'
+ * task when changes have been made
+ *
+ * Dependency: 'build' task
+ */
+gulp.task('watch:sass', ['build'],function () {
+  watch('client/**/*.scss', batch(function (events, done) {
+    gulp.start('sass', done);
+  }));
 });
+
+/**
+ * watch:ng
+ * Watches the AngularJS code and runs the
+ * 'default' task when changes have been made.
+ *
+ * Dependency: 'build' task
+ */
+gulp.task('watch:ng', ['build'],function () {
+  watch(['client/my-ng-files/**/*.ng.*.js','client/my-ng-files/**/*.ng.template.html'], batch(function (events, done) {
+    gulp.start('default', done);
+  }));
+});
+
+/**
+ * watch
+ * Task composed of other watch subtasks
+ */
+gulp.task('watch',['watch:sass','watch:ng']);
+
+//gulp.task('watch', ['scripts:watch', 'inject'], function () {
+//  gulp.watch([path.join(conf.paths.src, '/*.html'), 'bower.json'], ['inject-reload']);
+//
+//  gulp.watch([
+//    path.join(conf.paths.src, '/app/**/*.css'),
+//    path.join(conf.paths.src, '/app/**/*.scss')
+//  ], function(event) {
+//    if(isOnlyChange(event)) {
+//      gulp.start('styles-reload');
+//    } else {
+//      gulp.start('inject-reload');
+//    }
+//  });
+//
+//
+//  gulp.watch(path.join(conf.paths.src, '/app/**/*.html'), function(event) {
+//    browserSync.reload(event.path);
+//  });
+//});
